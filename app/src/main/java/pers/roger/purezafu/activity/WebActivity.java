a2692b051be918884af056923aefa389e1d1f935
rogeryoungh <rogeryoungh@foxmail.com>
2022-12-10 21:36:49 +0800

    update

--- app/src/main/java/pers/roger/purezafu/activity/WebActivity.java
+++ app/src/main/java/pers/roger/purezafu/activity/WebActivity.java
@@ -1,25 +1,70 @@
 package pers.roger.purezafu.activity;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
+import android.net.http.SslError;
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.GeolocationPermissions;
+import android.webkit.SslErrorHandler;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import java.lang.reflect.Method;
+
 import pers.roger.purezafu.R;
+import pers.roger.purezafu.util.LocationHelper;
 
 public class WebActivity extends AppCompatActivity {
+
+    public WebView webView;
+    public ConstraintLayout constraintLayout;
+    public String nowUrl = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_web);
 
-        WebView webView = findViewById(R.id.webview);
+        webView = findViewById(R.id.webview);
+        constraintLayout = findViewById(R.id.webview_layout);
 
         Intent intent = getIntent();
-        webView.loadUrl(intent.getStringExtra("url"));
+
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true); // 启用javascript
+        settings.setDomStorageEnabled(true); // 支持HTML5中的一些控件标签
+        settings.setBuiltInZoomControls(false); // 自选，非必要
+        //处理http和https混合的问题
+        settings.setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
+        // 允许javascript出错
+        try {
+            Method method = Class.forName("android.webkit.WebView").
+                    getMethod("setWebContentsDebuggingEnabled", Boolean.TYPE);
+            method.setAccessible(true);
+            method.invoke(null, true);
+        } catch (Exception e) {
+            // do nothing
+        }
+
+        webView.setFocusable(true); // 自选，非必要
+        webView.setDrawingCacheEnabled(true); // 自选，非必要
+        webView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY); // 自选，非必要
+        webView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public void onGeolocationPermissionsShowPrompt(String origin, GeolocationPermissions.Callback callback) {
+                super.onGeolocationPermissionsShowPrompt(origin, callback);
+                callback.invoke(origin, true, false); // 页面有请求位置的时候需要
+            }
+        });
+
+        webView.addJavascriptInterface(new LocationHelper(this), "zafu");
+
+
         webView.setWebViewClient(new WebViewClient() {
             @Override
             public boolean shouldOverrideUrlLoading(WebView view, String url) {
@@ -28,7 +73,19 @@ public class WebActivity extends AppCompatActivity {
                 //返回true
                 return true;
             }
+
+            @Override
+            public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
+                switch (error.getPrimaryError()) {
+                    case SslError.SSL_INVALID: // 校验过程遇到了bug
+                    case SslError.SSL_UNTRUSTED: // 证书有问题
+                        handler.proceed();
+                    default:
+                        handler.cancel();
+                }
+            }
         });
-//        webView.loadUrl(intent.getStringExtra("url"));
+        nowUrl = intent.getStringExtra("url");
+        webView.loadUrl(nowUrl);
     }
 }
---
+++ app/src/main/java/pers/roger/purezafu/util/LocationHelper.java
@@ -0,0 +1,64 @@
+package pers.roger.purezafu.util;
+
+import android.Manifest;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.util.Log;
+import android.webkit.JavascriptInterface;
+import android.webkit.ValueCallback;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import pers.roger.purezafu.activity.WebActivity;
+
+public class LocationHelper {
+    private WebActivity webActivity;
+
+    public LocationHelper(WebActivity webActivity) {
+        this.webActivity = webActivity;
+    }
+
+    public void eval(String js, ValueCallback<String> callback) {
+        webActivity.webView.evaluateJavascript("javascript:" + js, callback);
+    }
+
+    public void eval(String js) {
+        webActivity.webView.evaluateJavascript("javascript:" + js, v -> {
+        });
+    }
+
+    @JavascriptInterface
+    public void gdGetLocation() {
+        String manifests[] = {
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_BACKGROUND_LOCATION
+        };
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            webActivity.requestPermissions(manifests, 100);
+        }
+
+        if (webActivity.nowUrl.endsWith("/h5app/checkinclass.htm")) {
+            webActivity.runOnUiThread(() -> eval("$('.lat').text()", lat -> eval("$('.lng').text()", lng -> {
+                Snackbar
+                    .make(webActivity.constraintLayout, "上课啦签到：北纬" + lat + "东经：" + lng, 5000)
+                    .setAction("继续", v -> {
+                        String callback = "(s, r) => zafu.sendLocation(r.locations[0].lat, r.locations[0].lng)";
+                        String call = "AMap.convertFrom([" + lng + ", " + lat + "], 'gps', " + callback + ")";
+                        Log.i("zafu", call);
+                        eval(call);
+                    }).show();
+            })));
+        } else {
+            Snackbar.make(webActivity.constraintLayout, "未知定位使用", 5000).show();
+        }
+    }
+
+    @JavascriptInterface
+    public void sendLocation(String lat, String lng) {
+        String params = "\"success\", " + lat + ", " + lng;
+        Log.i("zafu", params);
+        webActivity.runOnUiThread(() -> eval("gdGetLocationCallback(" + params + ")"));
+    }
+}
